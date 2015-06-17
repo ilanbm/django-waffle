2205f1bb81c8c2c06074ecec10a1f77cc88b4341
Vladimir A Filonov <i@vladimir.filonov.name>
2015-06-17 17:09:21 +0300

    Fix tests and add migration for django > 1.6

--- waffle/tests/test_templates.py
+++ waffle/tests/test_templates.py
@@ -1,4 +1,6 @@
+from unittest import skipIf
 from django import template
+import django
 from django.contrib.auth.models import AnonymousUser
 from django.template import Template
 from django.template.base import VariableNode
@@ -48,6 +50,7 @@ class WaffleTemplateTests(TestCase):
         assert 'switch off' in content
         assert 'sample' in content
 
+    @skipIf(django.VERSION > (1, 6), "Skip jingo tests for django > 1.6")
     @override_settings(TEMPLATE_LOADERS=(
         'jingo.Loader',
         'django.template.loaders.filesystem.Loader',

4341faf0111e822f08944a8d0ac528df7c51b6da
kost <imvalgo@gmail.com>
2015-12-02 22:26:24 +0100

    New attribute "all_sites_overrid" which makes entity global across all sites. Default is True.

--- waffle/migrations/0006_auto_20150923_1331.py
+++ waffle/migrations/0006_auto_20150923_1331.py
@@ -14,16 +14,19 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='flag',
             name='site',
-            field=models.ManyToManyField(related_name='waffle_flags_m2m', to='sites.Site', blank=True),
+            field=models.ManyToManyField(related_name='waffle_flags_m2m', to='sites.Site', blank=True,
+                                         help_text=b'utilized only if `all_sites_override` is set to False'),
         ),
         migrations.AlterField(
             model_name='sample',
             name='site',
-            field=models.ManyToManyField(related_name='waffle_samples_m2m', to='sites.Site', blank=True),
+            field=models.ManyToManyField(related_name='waffle_samples_m2m', to='sites.Site', blank=True,
+                                         help_text=b'utilized only if `all_sites_override` is set to False'),
         ),
         migrations.AlterField(
             model_name='switch',
             name='site',
-            field=models.ManyToManyField(related_name='waffle_switches_m2m', to='sites.Site', blank=True),
+            field=models.ManyToManyField(related_name='waffle_switches_m2m', to='sites.Site', blank=True,
+                                         help_text=b'utilized only if `all_sites_override` is set to False'),
         ),
     ]
